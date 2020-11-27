49392ec6e7b04e34be00a3c87614a4fb895c49cd
vivek-appscms <vivek@appscms.com>
2020-11-27 21:26:07 +0530

    fix

--- createFile.rb
+++ createFile.rb
@@ -5,7 +5,7 @@ file_array = YAML.load(File.read("createFile.yml"))
 #   file_array.each { 
 #       |x| 
 #       File.open( "translate/#{x}.md", 'w' ) do |the_file|
-#           the_file.puts "--- \nlayout: translate \nfileName: translate_english_to_hindi \nfrom: en\nto: hi \npermalink: translate-#{x}\n---"
+#           the_file.puts "--- \nlayout: translate \nfileName: translate_english_to_hindi \nfrom: en\nto: hi \npermalink: translate-#{x}-sentence-translation\n---"
 
 #             end
 #       }
