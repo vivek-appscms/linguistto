dd6c1615fe3b5ba9148c3d88f32e0885f019ecf0
Manpreet_singh <ms4110415@gmail.com>
2021-07-07 15:07:48 +0530

    error fixing

--- functions/translation.js
+++ functions/translation.js
@@ -16,7 +16,8 @@ exports.handler = async function (event, context) {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Headers':
+        'Origin, X-Request-With, Content-Type , Accept',
     },
     body: JSON.stringify(response.data),
   }
